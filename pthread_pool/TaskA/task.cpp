ba437085f0900ed1ea800eba437beafd3f0003d6
Aether <2085163736@qq.com>
2025-02-01 17:18:13 +0800

    update files

--- pthread_pool/TaskA/task.cpp
+++ pthread_pool/TaskA/task.cpp
@@ -1,25 +1,49 @@
 #include<iostream>
-#include"threadpool.h"
+#include"threadpool"
+#include<sstream>
+#include<chrono>
+#include <boost/multiprecision/cpp_int.hpp>
 using namespace std;
+using namespace boost::multiprecision;
+using namespace chrono;
 
-unsigned long long Fact(int n) {
-    if(n<=1)return 1;
-    return n * Fact(n-1);
+// 祖传代码
+cpp_int c(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    return n * c(n - 1);
+}
+
+void Task(int n) {
+    ostringstream oss;
+    oss<<c(n);
+    char * result = new char[204800];
+    strcpy(result,oss.str().c_str());
+    printf("%d 的阶乘是 \n %s\n\n",n,result);
+    delete[] result;
 }
 
 int main()
 {
-    threadpool pool(6);
+    auto start = high_resolution_clock::now();
+    threadpool pool(24);
+
     
-    // for(int i = 1;i<20;i++) {
-    //     pool.TaskSubmit(
-    //         [i]() {
-    //             cout<<i<<endl;;
-    //         }
-    //     );
-    // }
+    for(int i = 1;i<=1000;i++) {
+        pool.submit([i]() {
+            Task(i);
+        });
+    }
 
-    pool.Stop();
-
+    pool.stop();
+    auto end = high_resolution_clock::now();
+    auto mstime = duration_cast<microseconds>(end - start);
+    auto stime = duration_cast<seconds>(end - start);
+    cout<<"共用时 "<<mstime.count()<<" ms"<<endl;
+    cout<<"约 "<<stime.count()<<" s"<<endl;
+    
     return 0;
 }
